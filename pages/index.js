9d320026bd396f4b1d37936d213533f69aeda003
Glen Barnhardt <barnent1@Glens-Mac-mini.local>
2022-01-08 15:39:14 -0500

    cleaned up

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Amplify } from "aws-amplify";
+import awsExports from "../src/aws-exports";
+
+Amplify.configure({ ...awsExports, ssr: true });
 
 export default function Home() {
   return (
@@ -60,7 +63,7 @@ export default function Home() {
         >
           Powered by{' '}
           <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+            <img src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
